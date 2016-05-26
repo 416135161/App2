e262c8f2809edb4658a43da06b1469a861f65530
416135161@qq.com <zj871125>
2016-05-26 18:16:31 +0800

    ftasdfasdf

--- src/com/sjning/app2/MainActivity.java
+++ src/com/sjning/app2/MainActivity.java
@@ -10,120 +10,129 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.sjning.app2.db.DBTool;
 import com.sjning.app2.receive.BootService;
 import com.sjning.app2.receive.MessageItem;
-import com.sjning.app2.tools.UserSession;
+import com.sjning.app2.tools.FileUtils;
 import com.sjning.app2.ui.TopBar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	private ListView listView;
-	
+	private View dataView;
 	private TextView noData;
-	
+	private Button sendBtn;
+
 	private MyAdapter adapter;
-	
+
 	private Handler mHandler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
 			// TODO Auto-generated method stub
 			super.handleMessage(msg);
 			adapter.notifyDataSetChanged();
-			if (adapter.getCount() == 0)
+			if (adapter.getCount() == 0) {
 				noData.setVisibility(View.VISIBLE);
-			else noData.setVisibility(View.INVISIBLE);
+				dataView.setVisibility(View.INVISIBLE);
+			} else {
+				noData.setVisibility(View.INVISIBLE);
+				dataView.setVisibility(View.VISIBLE);
+			}
 		}
 	};
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		setTopBar();
-		listView = (ListView)findViewById(R.id.listview);
+		dataView = findViewById(R.id.data_view);
+		sendBtn = (Button) findViewById(R.id.btn_send);
+		sendBtn.setOnClickListener(this);
+		listView = (ListView) findViewById(R.id.listview);
 		adapter = new MyAdapter();
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(new OnItemClickListener() {
-			
+
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
 				// TODO Auto-generated method stub
 				Intent intent = new Intent();
 				intent.setClass(MainActivity.this, MessageAct.class);
-				intent.putExtra("message", (MessageItem)adapter.getItem(arg2));
+				intent.putExtra("message", (MessageItem) adapter.getItem(arg2));
 				startActivity(intent);
 			};
-			
+
 		});
-		noData = (TextView)findViewById(R.id.nodata);
-		
+		noData = (TextView) findViewById(R.id.nodata);
+
 		startService(new Intent(this, BootService.class));
 		System.out.println("rrrrrrrrrrrrrrrrrrr");
 	}
-	
+
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
 		initListView();
 	}
-	
+
 	private void initListView() {
 		new Thread(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				List<MessageItem> items =
-					DBTool.getInstance().getSavedMessage(getApplicationContext(),
-						null,
-//						UserSession.getPhone(getApplicationContext())
-						null
-						);
+				List<MessageItem> items = DBTool.getInstance().getSavedMessage(
+						getApplicationContext(), null,
+						// UserSession.getPhone(getApplicationContext())
+						null);
 				if (items != null) {
 					adapter.setData(items);
 				}
 				mHandler.sendEmptyMessage(0);
 			}
-			
+
 		}).start();
 	}
-	
+
 	class MyAdapter extends BaseAdapter {
 		private List<MessageItem> items = new ArrayList<MessageItem>();
-		
+
 		public void setData(List<MessageItem> tasks) {
 			this.items.clear();
 			this.items.addAll(tasks);
 			tasks.clear();
 			tasks = null;
 		}
-		
+
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
 			return items.size();
 		}
-		
+
 		@Override
 		public Object getItem(int position) {
 			// TODO Auto-generated method stub
 			return items.get(position);
 		}
-		
+
 		@Override
 		public long getItemId(int position) {
 			// TODO Auto-generated method stub
 			return 0;
 		}
-		
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			// TODO Auto-generated method stub
@@ -133,23 +142,21 @@ public class MainActivity extends Activity {
 				LayoutInflater inflater = getLayoutInflater();
 				convertView = inflater.inflate(R.layout.message_item, null);
 				holder = new ViewHolder();
-				holder.text1 = (TextView)convertView.findViewById(R.id.text1);
-				holder.text2 = (TextView)convertView.findViewById(R.id.text2);
-				holder.text3 = (TextView)convertView.findViewById(R.id.text3);
-				
+				holder.text1 = (TextView) convertView.findViewById(R.id.text1);
+				holder.text2 = (TextView) convertView.findViewById(R.id.text2);
+
 				convertView.setTag(holder);
 			} else {
-				holder = (ViewHolder)convertView.getTag();
+				holder = (ViewHolder) convertView.getTag();
 			}
 			holder.text1.setText(item.getPhone());
-			holder.text2.setText(item.getBody());
-			holder.text3.setText(item.getDate().replace(" ", "\n "));
+			holder.text2.setText(item.getDate());
 			return convertView;
-			
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public void onBackPressed() {
 		// TODO Auto-generated method stub
@@ -158,19 +165,28 @@ public class MainActivity extends Activity {
 		// intent.setClass(this, Search.class);
 		// startActivity(intent);
 	}
-	
+
 	class ViewHolder {
 		TextView text1;
-		
 		TextView text2;
-		
-		TextView text3;
+
 	}
-	
+
 	private void setTopBar() {
-		TopBar topBar = (TopBar)findViewById(R.id.topBar);
+		TopBar topBar = (TopBar) findViewById(R.id.topBar);
 		topBar.hiddenLeftButton(true);
 		topBar.hiddenRightButton(true);
 		topBar.setTitle("甩手掌柜");
+	}
+
+	@Override
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+		case R.id.btn_send:
+			FileUtils.saveToSDCardOrRAM(this, filename, content, filePath);
+			break;
+
+		}
 	}
 }
